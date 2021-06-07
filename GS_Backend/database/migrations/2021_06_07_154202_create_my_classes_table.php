796a9de25dfcb769885f3c52147f0d094e4eb249
M M Rayhan Parvez <pecon.alok@gmail.com>
2021-06-07 22:54:13 +0600

    GS First Rest API

---
+++ GS_Backend/database/migrations/2021_06_07_154202_create_my_classes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMyClassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('my_classes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->integer('subject_id');
+            $table->integer('pupil_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('my_classes');
+    }
+}
---
+++ GS_Backend/database/migrations/2021_06_07_154353_create_tests_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tests', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamp('test_date')->nullable();
+            $table->integer('teacher_id');
+            $table->integer('student_id');
+            $table->integer('subject_id');
+            $table->float('grade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tests');
+    }
+}
